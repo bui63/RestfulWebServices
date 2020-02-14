424f7f5f2fb35ed561d63e45647a2581c0a67d13
Rahul Kumar Singh <singhrahulkr1002@gmail.com>
2020-02-14 10:53:02 +0530

    added method for list of filtering

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,13 +1,18 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class RestfulWebServicesApplicationTests {
+@SpringBootTest(classes = RestfulWebServicesApplication.class)
+@RunWith(SpringRunner.class)
+public class RestfulWebServicesApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		String[] args={"hi"};
+		RestfulWebServicesApplication.main(args);
 	}
 
 }
